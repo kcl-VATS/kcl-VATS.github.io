85ec275471f1e04511c4310d56d3b531436e6b30
kcl-vats <tunagonenpsn@gmail.com>
2022-06-06 12:26:24 +0100

    material components implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+
+
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
         <p>
-        Visual Analytics for Time-series (VATS)
+          Visual Analytics for Time-series (VATS)
         </p>
-        <a>
+        <p>
           The COVID-19 pandemic has been one of the biggest public health challenges we have faced in the last
           century.
           Uncertainty together with associated levels of risk has seen the development of one-of-a-kind initiatives,
@@ -30,11 +44,7 @@ function App() {
           mining + data visualization)
           tools to support data-intensive applications with focus on epidemic epidemiology modelling and human
           multi-omic data analytics.
-        </a>
-      </header>
-
-      <body>
-
+        </p>
         <p>
           The project will build on existing work developed as part of the <a
             href="https://sites.google.com/view/rampvis/">RAMPVIS</a> project
@@ -51,61 +61,106 @@ function App() {
           and improvement through code, data, case studies.
           Test
         </p>
+      </header>
 
-        <h2> People </h2>
+      <body>
 
-        <div>
-          <div>
+    
 
-            <img height="200" width="200" src={process.env.PUBLIC_URL + '/static/rita.jpg'} />
-            <p>
-              Rita Borgo is a Senior Lecturer in Data Visualization at the Informatics Department at King’s
-              College London (KCL),
-              Head of the Human Centred Computing research group, and Deputy Director of the Centre for Urban
-              Science and Progress (CUSP) - London.
-              Her research focus is on Information Visualization and Visual Analytics with a particular focus on
-              the role of Human Factors in Visualization.
+        <Container maxWidth="lg">
+          <Typography variant="h4">
+            People
+          </Typography>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <Box sx={{ marginLeft: "10px" }}>
+                  <Avatar sx={{ height: 100, width: 100 }} src={process.env.PUBLIC_URL + '/static/rita.jpg'} />
 
-            </p>
-          </div>
-          <div>
-            <img height="200" width="200" src={process.env.PUBLIC_URL + '/static/jordana.jpg'} />
-            <p>
-              Jordana Bell is a Reader in Epigenomics in the Department of Twin Research and Genetic Epidemiology.
-              She is currently coordinator of the international JPI HDHL ERA DIMENSION consortium,
-              through which large-scale multi-omic time-series datasets are being generated across multiple
-              cohorts, both in fasting and postprandial state.
-              JB is also using AI applications to reduce the high-dimensionality of human multi-omic biological
-              datasets in cardio-metabolic health research
-              .
-            </p>
-          </div>
-        </div>
-        <div >
-          <div>
-            <img height="200" width="200" src={process.env.PUBLIC_URL + '/static/alfie.jpg'} />
-            <p>
-              Alfie Abdul-Rahman is a Lecturer in Computer Science at the Informatics Department at King’s College
-              London (KCL).
-              Before joining King’s College London, she was a Research Associate at the University of Oxford
-              e-Research Centre.
-              She worked as a Research Engineer in HP Labs Bristol on document engineering, and then as a Software
-              Developer in London,
-              working on multi-format publishing.
-            </p>
-          </div>
-          <div>
-            <img height="200" width="200" src={process.env.PUBLIC_URL + '/static/tuna.jpg'} />
-            <p>
-              Tuna Gonen is a Research Software Engineer working on developing the online Visual Analytics tool
-              for analysing the time series data.
-              He graduated from University of Oxford with MEng. His previous work was developing VASAP (Visual
-              Analytics for Search, Analysis, and Prediction
-              with Epidemiological Time-series Data) as a part of RAMPVIS
-            </p>
-          </div>
-        </div>
+                </Box>
+                <CardActionArea>
+
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Rita Borgo is a Senior Lecturer in Data Visualization at the Informatics Department at King’s
+                      College London (KCL),
+                      Head of the Human Centred Computing research group, and Deputy Director of the Centre for Urban
+                      Science and Progress (CUSP) - London.
+                      Her research focus is on Information Visualization and Visual Analytics with a particular focus on
+                      the role of Human Factors in Visualization.
+
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <Box sx={{ marginLeft: "10px" }}>
+                  <Avatar sx={{ height: 100, width: 100 }} src={process.env.PUBLIC_URL + '/static/jordana.jpg'} />
+
+                </Box>
+                <CardActionArea>
+
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Jordana Bell is a Reader in Epigenomics in the Department of Twin Research and Genetic Epidemiology.
+                      She is currently coordinator of the international JPI HDHL ERA DIMENSION consortium,
+                      through which large-scale multi-omic time-series datasets are being generated across multiple
+                      cohorts, both in fasting and postprandial state.
+                      JB is also using AI applications to reduce the high-dimensionality of human multi-omic biological
+                      datasets in cardio-metabolic health research.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+
+              </Card>
+            </Grid>    <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <Box sx={{ marginLeft: "10px" }}>
+                  <Avatar sx={{ height: 100, width: 100 }} src={process.env.PUBLIC_URL + '/static/alfie.jpg'} />
+
+                </Box>
+                <CardActionArea>
+
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Alfie Abdul-Rahman is a Lecturer in Computer Science at the Informatics Department at King’s College
+                      London (KCL). Before joining King’s College London, she was a Research Associate at the University of Oxford
+                      e-Research Centre.
+                      She worked as a Research Engineer in HP Labs Bristol on document engineering, and then as a Software
+                      Developer in London,
+                      working on multi-format publishing.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+
+              </Card>
+            </Grid>    <Grid item xs={12} sm={6} md={4}>
+              <Card>
+                <Box sx={{ marginLeft: "10px" }}>
+                  <Avatar sx={{ height: 100, width: 100 }} src={process.env.PUBLIC_URL + '/static/tuna.jpg'} />
+
+                </Box>
+                <CardActionArea>
+
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Tuna Gonen is a Research Software Engineer working on developing the online Visual Analytics tool
+                      for analysing the time series data. He graduated from University of Oxford with MEng.
+                      His previous work was developing VASAP (VisualAnalytics for Search, Analysis, and
+                      Prediction with Epidemiological Time-series Data) as a part of RAMPVIS.
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
       </body>
+
     </div>
   );
 }
